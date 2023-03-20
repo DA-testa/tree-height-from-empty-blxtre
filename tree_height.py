b9ee38796d63290f884eca5888f6b9d4a54b4112
blxtre <127733096+blxtre@users.noreply.github.com>
2023-03-20 12:37:36 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,61 +1,38 @@
-# python3
-
 import sys
 import threading
 import numpy
 
-
-max_height = 0
-    
-    nodes = [0] * n
-    
+def compute_height(n, parents):
+    # Write this function
+    max_height = 0
+    # Create an array to hold each node's height
+    heights = [0] * n 
+    # Iterate through the parents array
     for i in range(n):
-        
-        nodes[i] = parents[i]
-    
-    root = nodes[0]
-    
-    for i in range(n):
-        
-        height = 1
-        
-        curr_node = nodes[i]
-        
-        while (curr_node != root):
-            
-            curr_node = nodes[curr_node]
-            
-            height += 1
-        
-        max_height = max(max_height, height)
-
+        # Check if the parent of the current node is the root node
+        # If so, mark it as the height of the tree
+        if parents[i] == -1:
+            max_height = 1
+        # Otherwise, calculate the height of the current node
+        else:
+            heights[i] = heights[parents[i]] + 1 
+            # Check if the height of the current node is greater than the current maximum height
+            if heights[i] > max_height:
+                max_height = heights[i]
     return max_height
 
-
 def main():
-   
-    filename = input("Enter filename: ")
-    
-    if "a" in filename:
-        print("Filename cannot contain letter a")
-        return
-    
-    file = open(filename, "r")
-    
-    lines = file.readlines()
-    
-    n = int(lines[0])
- 
-    parents = [int(x) for x in lines[1].split()]
-    
+    # Read the number of elements from the input
+    n = int(input())
+    # Read the elements from the input
+    parents = [int(x) for x in input().split()]
+    # Call the function and output the result
     print(compute_height(n, parents))
 
-if __name__ == "__main__":
-    # In Python, the default limit on recursion depth is rather low,
-    # so raise it here for this problem. Note that to take advantage
-    # of bigger stack, we have to launch the computation in a new thread.
-    sys.setrecursionlimit(10**7)  # max depth of recursion
-    threading.stack_size(2**27)   # new thread will get stack of such size
-    threading.Thread(target=main).start()
+# In Python, the default limit on recursion depth is rather low,
+# so raise it here for this problem. Note that to take advantage
+# of bigger stack, we have to launch the computation in a new thread.
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)   # new thread will get stack of such size
+threading.Thread(target=main).start()
 main()
-# print(numpy.array([1,2,3]))
